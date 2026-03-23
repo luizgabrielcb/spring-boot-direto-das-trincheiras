88f064df6a92c1e4350703423e3cb17373ea1d30
Luiz Gabriel <luiz.gabrielcb2006@gmail.com>
2026-03-23 16:06:40 -0300

    video06: @GetMapping and @RestController

--- anime-service/src/main/java/academy/devdojo/AnimeServiceApplication.java
+++ anime-service/src/main/java/academy/devdojo/AnimeServiceApplication.java
@@ -3,11 +3,14 @@ package academy.devdojo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class AnimeServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AnimeServiceApplication.class, args);
+        var applicationContext = SpringApplication.run(AnimeServiceApplication.class, args);
+		Arrays.stream(applicationContext.getBeanDefinitionNames()).forEach(System.out::println);
 	}
 
 }
