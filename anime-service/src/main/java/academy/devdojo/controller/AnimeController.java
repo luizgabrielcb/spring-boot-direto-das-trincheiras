f5f39d94f962c78f3d7d2a43dfe2293b4d22308d
Luiz Gabriel <luiz.gabrielcb2006@gmail.com>
2026-03-24 20:58:21 -0300

    video13: Exercise RequestParam and PathVariable

--- anime-service/src/main/java/academy/devdojo/controller/AnimeController.java
+++ anime-service/src/main/java/academy/devdojo/controller/AnimeController.java
@@ -1,22 +1,28 @@
 package academy.devdojo.controller;
 
+import academy.devdojo.domain.Anime;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("v1/animes")
 @Slf4j
 public class AnimeController {
 
-    @GetMapping
-    public List<String> listAll() throws InterruptedException {
-        log.info(Thread.currentThread().getName());
-        TimeUnit.SECONDS.sleep(1);
-        return List.of("Drogon Ball Z", "Kimetsu no Yaiba", "Shingeki no Kyojin");
+    @GetMapping()
+    public List<Anime> listAllAnimes() {
+        return Anime.getAnimes();
+    }
+
+    @GetMapping("filterParam")
+    public List<Anime> listAllAnimesParam(@RequestParam String name) {
+        return Anime.getAnimes().stream().filter(anime -> anime.getName().equalsIgnoreCase(name)).toList();
+    }
+
+    @GetMapping("{id}")
+    public Anime listAllAnimesPathVariable(@PathVariable Long id) {
+        return Anime.getAnimes().stream().filter(anime -> anime.getId().equals(id)).findFirst().orElse(null);
     }
 }
