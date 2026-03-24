b492df53c3b040b1b059bbef1fbaa3dcbdd375de
Luiz Gabriel <luiz.gabrielcb2006@gmail.com>
2026-03-24 20:58:21 -0300

    video12: Exercise RequestParam and PathVariable

---
+++ anime-service/src/main/java/academy/devdojo/domain/Anime.java
@@ -0,0 +1,22 @@
+package academy.devdojo.domain;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Builder
+@Data
+public class Anime {
+    private Long id;
+    private String name;
+
+    public static List<Anime> getAnimes() {
+        List<Anime> animeList = new ArrayList<>();
+        animeList.add(Anime.builder().id(1L).name("Dragon Ball Z").build());
+        animeList.add(Anime.builder().id(2L).name("Kimetsu no Yaiba").build());
+        animeList.add(Anime.builder().id(3L).name("Re:Zero").build());
+        return animeList;
+    }
+}
