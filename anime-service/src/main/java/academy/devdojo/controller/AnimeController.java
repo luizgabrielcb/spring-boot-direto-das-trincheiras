92ed376564a6b92db4f79afbcb6ff3e4da68640b
Luiz Gabriel <luiz.gabrielcb2006@gmail.com>
2026-03-30 15:57:18 -0300

    video24: PutMapping

--- anime-service/src/main/java/academy/devdojo/controller/AnimeController.java
+++ anime-service/src/main/java/academy/devdojo/controller/AnimeController.java
@@ -3,6 +3,7 @@ package academy.devdojo.controller;
 import academy.devdojo.domain.Anime;
 import academy.devdojo.mapper.AnimeMapper;
 import academy.devdojo.request.AnimePostRequest;
+import academy.devdojo.request.AnimePutRequest;
 import academy.devdojo.response.AnimeGetResponse;
 import academy.devdojo.response.AnimePostResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -64,6 +65,22 @@ public class AnimeController {
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Anime not Found"));
 
         Anime.getAnimes().remove(animeToDelete);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> update(@RequestBody AnimePutRequest request) {
+        log.info("Request to update anime: {}", request);
+
+        var animeToDelete = Anime.getAnimes().stream()
+                .filter(anime -> anime.getId().equals(request.getId()))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Anime not Found"));
+
+        var animeToUpdate = MAPPER.toAnime(request);
+        Anime.getAnimes().remove(animeToDelete);
+        Anime.getAnimes().add(animeToUpdate);
 
         return ResponseEntity.noContent().build();
     }
---
+++ anime-service/src/main/java/academy/devdojo/request/AnimePutRequest.java
@@ -0,0 +1,9 @@
+package academy.devdojo.request;
+
+import lombok.Data;
+
+@Data
+public class AnimePutRequest {
+    private Long id;
+    private String name;
+}
