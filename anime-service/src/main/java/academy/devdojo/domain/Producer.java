e7fbbe86645f75f3b340aa5c9643ee0eba45d26b
Luiz Gabriel <luiz.gabrielcb2006@gmail.com>
2026-03-30 16:57:29 -0300

    video26: Created package repository (MVC)

--- anime-service/src/main/java/academy/devdojo/domain/Producer.java
+++ anime-service/src/main/java/academy/devdojo/domain/Producer.java
@@ -4,11 +4,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Builder
 @Data
@@ -18,14 +15,5 @@ public class Producer {
     @JsonProperty("name")
     private String name;
     private LocalDateTime createdAt;
-    @Getter
-    private static List<Producer> producers = new ArrayList<>();
-
-    static {
-        var mappa = Producer.builder().id(1L).name("Mappa").createdAt(LocalDateTime.now()).build();
-        var kyotoAnimation = Producer.builder().id(2L).name("Kyoto Animation").createdAt(LocalDateTime.now()).build();
-        var madhouse = Producer.builder().id(3L).name("Madhouse").createdAt(LocalDateTime.now()).build();
-        producers.addAll(List.of(mappa, kyotoAnimation, madhouse));
-    }
 
 }
