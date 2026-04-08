87000a0043c2c80ee49732ec35ae02ae43e7068d
Luiz Gabriel <luiz.gabrielcb2006@gmail.com>
2026-04-08 17:19:22 -0300

    video49: Spring Profile pt01 Value

--- anime-service/src/main/java/external/dependency/Connection.java
+++ anime-service/src/main/java/external/dependency/Connection.java
@@ -1,10 +1,12 @@
 package external.dependency;
 
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 @RequiredArgsConstructor
 @ToString
+@Data
 public class Connection {
     private final String host;
     private final String username;
