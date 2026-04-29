0de0ae4957c876ab37114eae86785e441fe2c4ba
Luiz Gabriel <luiz.gabrielcb2006@gmail.com>
2026-04-29 10:47:28 -0300

    video161: add integration tests for BrasilApiController and configure WireMock

---
+++ user-service/src/test/java/academy/devdojo/controller/BrasilApiControllerIT.java
@@ -0,0 +1,14 @@
+package academy.devdojo.controller;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.test.context.jdbc.Sql;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(value = "/sql/init_one_login_regular_user.sql")
+@AutoConfigureWireMock(port = 0, files = "classpath:/wiremock/brasil-api/cep", stubs = "classpath:/wiremock/brasil-api/mappings")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class BrasilApiControllerIT {
+}
