f31213c9aecf5b8550a89d48c1e89e081193404c
Luiz Gabriel <luiz.gabrielcb2006@gmail.com>
2026-04-29 16:04:14 -0300

    video165: add Flyway integration and initial database setup for anime service

---
+++ anime-service/src/main/resources/db/migration/V2__initialize_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO anime_service.anime (id, name) VALUES (1, 'Jujutsu Kaisen');
+INSERT INTO anime_service.anime (id, name) VALUES (2, 'Kimi no Nawa');
+INSERT INTO anime_service.anime (id, name) VALUES (3, 'Attack on Titan');
+INSERT INTO anime_service.anime (id, name) VALUES (4, 'Re Zero');
